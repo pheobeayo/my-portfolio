af237608ee25569e9e7a386b371aa855df3d4b76
Ifeoluwa Sanni <47613365+pheobeayo@users.noreply.github.com>
2023-06-23 12:48:52 +0100

    initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,170 @@
+import React from "react";
+
+
+export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
+  return (
+    <section id="contact" className="relative">
+
+      <form
+        netlify
+        name="contact"
+        onSubmit={handleSubmit}
+        className="py-10 px-10 flex flex-col  bg-grey rounded-xl shadow-lg space-y-2 md:py-6  md:items-center md:space-y-0 md:space-x-8">
+        <h2 className="text-white md:text-6xl text-5xl mb-1 font-large title-font">
+          Hire Me
+        </h2>
+        <p className="leading-relaxed mb-5">
+          Do you need a Collaboration or require a software developer for any project?
+          <br className="hidden lg:inline-block" /> 
+        </p>
+        <div className="relative mb-4">
+          <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="relative mb-4">
+          <label
+            htmlFor="message"
+            className="leading-7 text-sm text-gray-400">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Submit
+        </button>
+      </form>
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+
+          <div className="bg-gray-900 relative flex flex-wrap py-8 rounded shadow-md">
+            <div className="lg:w- px-6 mt-4 lg:mt-0">
+             
+
+             <a href="https://www.linkedin.com/in/ifeoluwa-sanni-569979151/" target="">
+              <button
+                type="button"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+                class="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                </svg>
+              </button>
+              </a>
+              
+              <a href="https://web.whatsapp.com/" target="">
+              <button
+                type="button"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+                class="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+                </svg>
+              </button>
+              </a>
+
+              <a href="https://github.com/pheobeayo">
+              <button
+                type="button"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+                class="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </button>
+              </a>
+
+            <a href="https://twitter.com/pheobeayolove1" target= "">
+              <button
+                type="button"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+                class="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                </svg>
+              </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const projects = [
+  {
+    title: "E-commerce",
+    subtitle: "JS Mastery Ecommerce",
+    description:
+      "An e-commerce website",
+    image: "./project-1.jpeg",
+    link: "https://ecommerce-pheobeayo.vercel.app/",
+  },
+  {
+    title: "Vetly",
+    subtitle: "Vetting App",
+    description:
+      "A Talent Vetting App",
+    image: "./project-2.jpeg",
+    link: "https://dev-vetlyapp.netlify.app/",
+  },
+  {
+    title: "simple Redux App",
+    subtitle: "React and Redux",
+    description:
+      "A simple redux app for data",
+    image: "./project-3.jpeg",
+    link: "https://simple-redux-app.netlify.app/",
+  },
+  {
+    title: "My NFT collections",
+    subtitle: "NFT",
+    description:
+      "An NFT collections website",
+    image: "./project-4.jpeg",
+    link: "https://my-nft-collection.pheobeayo.repl.co",
+  },
+  {
+    title: "Krypto staking",
+    subtitle: "KRYPTO",
+    description:
+      "A Defi Staking Application",
+    image: "./project-5.jpg",
+    link: "https://krypto-frontend-snowy.vercel.app/",
+  },
+  {
+    title: "Qualify",
+    subtitle: "A blockchain Vetting App",
+    description:
+      "An Application that vets talents",
+    image: "./project-6.jpg",
+    link: "https://qualify-rosy.vercel.app/",
+  },
+  {
+    title: "GoldCart",
+    subtitle: "An Ecommerce Website",
+    description:
+      "An e-commerce application that sells digital products",
+    image: "./project-7.jpg",
+    link: "https://goldcart.netlify.app/",
+  },
+];
+
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "Chakra UI",
+  "Redux",
+  "React-Query",
+  "HTML-5",
+  "CSS",
+  "Python"
+];
